7a22dc7c59a97e7dbf5a514ddd7b81e7dd4a4181
ceccs17d35 <43230115+ceccs17d35@users.noreply.github.com>
2018-12-01 00:00:46 +0530

    Update and rename queuearray.c to 26_queuearray.c

---
+++ 26_queuearray.c
@@ -0,0 +1,124 @@
+	//NAME        :MONIYA MOHAN
+	//ROLL NO     :35
+	//PROGRAM NO  :26
+	//PROGRAM NAME:QUEUE USING ARRAY
+
+	#include <stdio.h>
+
+	int queue[100], n=100, front=-1, rear=-1;
+
+	void enqueue() {
+		int data;
+		if (rear == n-1) {
+			printf("Failed: Queue full\n");
+		} else {
+			printf("Enter data to be entered : ");
+			scanf("%d", &data);
+			if (front == -1) {
+				front = rear = 0;
+			} else {
+				rear++;
+			}
+			queue[rear] = data;
+		}
+	}
+	void dequeue() {
+		if (front == -1) {
+			printf("Failed: Empty queue\n");
+		} else {
+			printf("Deleted element : %d\n", queue[front]);
+			if (front == rear) {
+				front = rear = -1;
+			} else {
+				front++;
+			}
+		}
+	}
+	void printQueue() {
+		int i=0;
+		if (front == -1) {
+			printf("Empty Queue\n");
+		} else {
+			for (i=front; i<=rear; i++) {
+				printf("%d\n", queue[i]);
+			}
+		}
+	}
+	int main() {
+		char choice;
+		front = rear = -1;
+		do {
+			printf("\n\tQueue using array\n1.Enqueu\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
+			scanf(" %c", &choice);
+			switch(choice) {
+				case '1':
+					enqueue();
+					break;
+				case '2':
+					dequeue();
+					break;
+				case '3':
+					printQueue();
+					break;
+			}
+		} while(choice != '4');
+	}
+
+	OUTPUT :
+
+	Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 1
+		Enter data to be entered : 1
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 1
+		Enter data to be entered : 2
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 1
+		Enter data to be entered : 3
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 1
+		Enter data to be entered : 5
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 2
+		Deleted element : 1
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 3
+		2
+		3
+		5
+
+			Queue using array
+		1.Enqueu
+		2.Dequeue
+		3.Display
+		4.Exit
+		Enter your choice : 4
