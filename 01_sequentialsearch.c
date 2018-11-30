48381b05b8141501346db4c062ee7b849023ec09
ceccs17d35 <43230115+ceccs17d35@users.noreply.github.com>
2018-11-30 10:45:28 +0530

    Update 01_sequentialsearch.c

--- 01_sequentialsearch.c
+++ 01_sequentialsearch.c
@@ -1,34 +1,52 @@
-#include <stdio.h>
+	//NAME        :MONIYA MOHAN
+	//ROLL NO     :35
+	//PROGRAM NO  :1
+	//PROGRAM NAME:SEQUENTIAL SEARCH
 
-int sequentialSearch(int array[], int size, int element) {
-	int i;
+	#include <stdio.h>
 
-	for (i=0; i<size; i++) {
-		if (array[i] == element) {
-			return (i+1);
+	int sequentialSearch(int array[], int size, int element) {
+		int i;
+
+		for (i=0; i<size; i++) {
+			if (array[i] == element) {
+				return (i+1);
+			}
+		}
+		return (0);
+	}
+	int main() {
+		int i=0, found=0, array[100], size, element;
+
+		printf("Enter number of elements : ");
+		scanf("%d", &size);
+
+		printf("Enter elements\n");
+		for (i=0; i<size; i++) {
+			scanf("%d", &array[i]);
+		}
+
+		printf("Enter element to search : ");
+		scanf("%d", &element);
+
+		found = sequentialSearch(array, size, element);
+
+		if (found) {
+			printf("Element found at position %d\n", found);
+		} else {
+			printf("Element not found\n");
 		}
 	}
-	return (0);
-}
-int main() {
-	int i=0, found=0, array[100], size, element;
 
-	printf("Enter number of elements : ");
-	scanf("%d", &size);
+	OUTPUT :
 
-	printf("Enter elements\n");
-	for (i=0; i<size; i++) {
-		scanf("%d", &array[i]);
-	}
+	Enter number of elements : 5
+	Enter elements
+	12
+	41
+	32
+	65
+	23
+	Enter element to search : 41
+	Element found at position 2
 
-	printf("Enter element to search : ");
-	scanf("%d", &element);
-
-	found = sequentialSearch(array, size, element);
-
-	if (found) {
-		printf("Element found at position %d\n", found);
-	} else {
-		printf("Element not found\n");
-	}
-}
